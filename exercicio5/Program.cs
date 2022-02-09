a4063a6dd90240863ee268b05cb266bd71e9d98d
nicol <nicol@DESKTOP-0809H1K>
2022-02-09 17:21:09 -0300

    Add project files.

---
+++ exercicio5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
